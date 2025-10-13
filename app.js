c0ff93784f4cd4dc7b3154ad3266dc01452cb639
Obertsean111 <Oberts1310@gmail.com>
2025-10-13 08:29:29 +0700

    update bagian app.js

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 import express from "express";
 
+import web from "./routes/web.js";
+import api from "./routes/api.js";
+import { database } from "./config/database.js";
+
 const app = express();
 
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {});
+
+app.use(web);
+app.use("/api", api);
+
 app.listen(3000, () => {
-    console.log(`Server berhasil berjalan di http://localhost:3000`);
+    database();
+    console.log(`Aplikasi sedang berjalan di http://localhost:3000`);
 });
